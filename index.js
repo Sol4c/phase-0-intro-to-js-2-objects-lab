8dfac892a3bf031f810b4078f2aa23f98e198a85
Sol4c <brownarmani86@yahoo.com>
2023-06-24 22:31:31 -0400

    sigh

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    name: "value",
+    streetAddress: "2D"
+    
+}
+function updateEmployeeWithKeyAndValue(object, key, value){
+    const copyOfObject = {...object}
+    copyOfObject[key] = value
+    return copyOfObject
+    
+
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value){
+    object[key] = value
+    return object
+}
+
+function deleteFromEmployeeByKey(object, key, value){
+    const newObj = {...object}
+    newObj[key] = value
+    return newObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key, value){
+    object[key] = value
+    return object
+}
